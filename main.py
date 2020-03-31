5ad89d8ffb9ef07d0589e8e4591a78544c21f0ec
len <l.yoshida.132@gmail.com>
2020-03-31 16:33:07 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from BlockChain import *
+if __name__ == '__main__':
+    bc = BlockChain(INITIAL_BITS)
+    print('generating genesis block ...')
+    bc.create_genesis()
+    for i in range(30):
+        print('generating {}th block ...'.format(i+2))
+        bc.add_new_block(i)
